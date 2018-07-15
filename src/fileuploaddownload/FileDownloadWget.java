163a48d0570c438af5c019bb74e849a779be3796
Mehavarnan <meha10aug@gmail.com>
2018-07-16 00:04:47 +0530

    update

--- src/fileuploaddownload/FileDownloadWget.java
+++ src/fileuploaddownload/FileDownloadWget.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class FileDownload {
+public class FileDownloadWget {
 
 	public static void main(String[] args) {
 
