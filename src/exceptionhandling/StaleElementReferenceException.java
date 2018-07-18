37683b89993d98793fde4f6ad23f84a0f9a303bd
Mehavarnan <meha10aug@gmail.com>
2018-07-18 23:04:35 +0530

    Added StaleElementhandling

---
+++ src/exceptionhandling/StaleElementReferenceException.java
@@ -0,0 +1,42 @@
+package exceptionhandling;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class StaleElementReferenceException {
+
+	public static void main(String[] args) {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+		
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+		
+		WebElement username = driver.findElement(By.id("txtUsername"));
+		WebElement password = driver.findElement(By.id("txtPassword"));
+		WebElement loginButton = driver.findElement(By.id("btnLogin"));
+				
+		username.sendKeys("admin");
+		password.sendKeys("admin");
+		loginButton.click();
+		
+		driver.navigate().refresh();
+		
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+		boolean stale = wait.until(ExpectedConditions.stalenessOf(username));
+		
+		if(stale) {
+			username = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("txtUsername")));
+		}
+		
+		username.sendKeys("Admin");
+	}
+
+}
